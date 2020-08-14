fb78fb3a3bd7768535b37a2fa6069e31f6bb1b06
emilioSp <emilio.spatola@gmail.com>
2020-08-14 11:26:44 +0200

    WIP

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const init = async () => {
+  // Simulate start delay
+  await sleep(5000);
+  const server = http.createServer( async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const response = {
+      status: 'OK',
+      date: new Date().toISOString()
+    }
+    res.end(JSON.stringify(response));
+  });
+
+  server.listen(80);
+  console.log('Server started');
+}
+
+init();
